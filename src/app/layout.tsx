68f25c39e0af4e5603c4e9a2910352208db6bfc1
bieniucieniu <mikibien@outlook.com>
2023-05-23 20:25:08 +0200

    remove inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
-import './globals.css'
-import { Inter, Roboto } from 'next/font/google'
+import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
-const roboto = Roboto({weight: "400", subsets: ['latin']})
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
-  title: 'bracia bien',
-  description: '',
-}
+  title: "bracia bien",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
