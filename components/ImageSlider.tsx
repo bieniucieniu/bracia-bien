2a9def53a64637b8de5709f4db324814fab0ed8e
bieniucieniu <mikibien@outlook.com>
2023-07-03 18:51:38 +0200

    fix bouncing on switch

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -3,12 +3,29 @@ import { useEffect, useReducer } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import { twMerge } from "tailwind-merge"
 import Image from "next/image"
-import { warn } from "console"
 const ImgPaths = [
   "/images/slider/fotob-33.jpg",
   "/images/slider/fotob-39.jpg",
   "/images/slider/fotob-40.jpg",
 ]
+
+const variants = {
+  enter: {
+    zindex: 0,
+    x: 1920,
+    opacity: 0,
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: {
+    zIndex: 0,
+    x: -1920,
+    opacity: 0,
+  },
+}
 
 function reducer(idx: number, action: "next" | "previous"): number {
   switch (action) {
@@ -29,7 +46,7 @@ export default function ImageSlider({ className }: { className?: string }) {
   const [idx, setIdx] = useReducer(reducer, 0)
 
   useEffect(() => {
-    const interval = setInterval(() => setIdx("next"), 1000 * 5)
+    const interval = setInterval(() => setIdx("next"), 1000 * 6)
     return () => clearInterval(interval)
   }, [idx])
 
@@ -37,7 +54,18 @@ export default function ImageSlider({ className }: { className?: string }) {
     <AnimatePresence>
       {
         ImgPaths.map((path, i) => (
-          <motion.div key={`imgs-slid-${i}`} className={twMerge(className)}>
+          <motion.div
+            key={`imgs-slid-${i}`}
+            className={twMerge(className, "absolute inset-0")}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+          >
             <Image
               src={path}
               alt={`emgs-slid-${i}`}
