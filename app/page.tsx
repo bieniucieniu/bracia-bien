5ca6dbb3988fde950d09b9e314ee7417f42665a0
bieniucieniu <mikibien@outlook.com>
2023-07-06 10:33:41 +0200

    remove class

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/MainNavbar"
 export default function Home() {
   return (
     <main style={{ scrollbarGutter: "stable" }}>
-      <div className="flex flex-col overflow-x-hidden min-h-screen">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
         <ImageSlider className="flex-1" />
       </div>
