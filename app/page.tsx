fe0caabbb12994010d6820d3de26c5a826e23563
bieniucieniu <mikibien@outlook.com>
2023-07-13 15:13:39 +0200

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -4,54 +4,54 @@ import InfoCards from "@/components/InfoCards"
 import Navbar from "@/components/MainNavbar"
 
 const imgPaths: string[] = [
-    "/images/fotob-33.jpg",
-    "/images/fotob-39.jpg",
-    "/images/fotob-40.jpg",
+	"/images/fotob-33.jpg",
+	"/images/fotob-39.jpg",
+	"/images/fotob-40.jpg",
 ]
 
 const cardsData: Parameters<typeof InfoCards>[0] = {
-    cards: [{ className: "m-auto" }],
+	cards: [{ className: "m-auto" }],
 }
 
 const contactsData: Parameters<typeof Contacts>[0] = {
-    title: (
-        <>
-            Skontaktuj się
-            <br /> z nami
-        </>
-    ),
-    contacts: [
-        { id: "tel", name: "+48 502 896 299", href: "tel:+48502896299" },
-        {
-            id: "mail",
-            name: "braciabien@gmail.com",
-            href: "mailto:braciabien@gmail.com",
-        },
-        {
-            id: "adress",
-            name: "Stawiszyńska 125, 62-800 Kalisz",
-            href: "https://goo.gl/maps/BfMbTwFQTeVjVR717",
-            newTab: true,
-        },
-    ],
+	title: (
+		<>
+			Skontaktuj się
+			<br /> z nami
+		</>
+	),
+	contacts: [
+		{ id: "tel", name: "+48 502 896 299", href: "tel:+48502896299" },
+		{
+			id: "mail",
+			name: "braciabien@gmail.com",
+			href: "mailto:braciabien@gmail.com",
+		},
+		{
+			id: "adress",
+			name: "Stawiszyńska 125, 62-800 Kalisz",
+			href: "https://goo.gl/maps/BfMbTwFQTeVjVR717",
+			newTab: true,
+		},
+	],
 }
 
 export default function Home() {
-    return (
-        <main style={{ scrollbarGutter: "stable" }}>
-            <div className="flex flex-col min-h-screen relative">
-                <Navbar />
-                <ImageSlider className="flex-1" imgPaths={imgPaths} />
-            </div>
-            <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-                <div className="bg-orange-50"></div>
-                <div className="bg-red-600 flex p-10">
-                    <InfoCards {...cardsData} />
-                </div>
-            </div>
-            <footer className="min-h-screen grid grid-cols-1 xl:grid-cols-2">
-                <Contacts {...contactsData} />
-            </footer>
-        </main>
-    )
+	return (
+		<main style={{ scrollbarGutter: "stable" }}>
+			<div className="flex flex-col min-h-screen relative bg-yellow-200">
+				<Navbar />
+				<ImageSlider className="flex-1" imgPaths={imgPaths} />
+			</div>
+			<div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+				<div className="bg-orange-50"></div>
+				<div className="bg-red-600 flex p-10">
+					<InfoCards {...cardsData} />
+				</div>
+			</div>
+			<footer className="min-h-screen grid grid-cols-1 xl:grid-cols-2">
+				<Contacts {...contactsData} />
+			</footer>
+		</main>
+	)
 }
--- components/InfoCards.tsx
+++ components/InfoCards.tsx
@@ -1,49 +1,49 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card"
 import { ReactNode } from "react"
 
 function InfoCard({
-    className,
-    title,
-    description,
-    children,
-    footer,
+	className,
+	title,
+	description,
+	children,
+	footer,
 }: {
-    className?: string
-    title?: string
-    description?: string
-    children?: ReactNode
-    footer?: ReactNode
+	className?: string
+	title?: string
+	description?: string
+	children?: ReactNode
+	footer?: ReactNode
 }) {
-    return (
-        <Card className={className}>
-            <CardHeader>
-                <CardTitle>{title}</CardTitle>
-                <CardDescription>{description}</CardDescription>
-            </CardHeader>
-            <CardContent>{children}</CardContent>
-            <CardFooter>{footer}</CardFooter>
-        </Card>
-    )
+	return (
+		<Card className={className}>
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+				<CardDescription>{description}</CardDescription>
+			</CardHeader>
+			<CardContent>{children}</CardContent>
+			<CardFooter>{footer}</CardFooter>
+		</Card>
+	)
 }
 export default function InfoCards({
-    cards,
-    className,
+	cards,
+	className,
 }: {
-    className?: string
-    cards: {
-        className?: string
-        title?: string
-        description?: string
-        children?: ReactNode
-        footer?: ReactNode
-    }[]
+	className?: string
+	cards: {
+		className?: string
+		title?: string
+		description?: string
+		children?: ReactNode
+		footer?: ReactNode
+	}[]
 }) {
-    return cards.map((e, i) => <InfoCard key={i} {...e} />)
+	return cards.map((e, i) => <InfoCard key={i} {...e} />)
 }
