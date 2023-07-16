7f5b2f11e94f2ee1b39b7fed9b218fe76f8f795c
bieniucieniu <mikibien@outlook.com>
2023-07-16 19:19:16 +0200

    add slides animation dep on direction

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -4,78 +4,109 @@ import { AnimatePresence, motion } from "framer-motion"
 import Image from "next/image"
 import { twMerge } from "tailwind-merge"
 const variants = {
-	enter: {
-		zindex: 0,
-		x: 1920,
-		opacity: 0,
-	},
-	center: {
-		zIndex: 1,
-		x: 0,
-		opacity: 1,
-	},
-	exit: {
-		zIndex: 0,
-		x: -1920,
-		opacity: 0,
-	},
+    right: {
+        enter: {
+            zindex: 0,
+            x: 1920,
+            opacity: 0,
+        },
+        center: {
+            zIndex: 1,
+            x: 0,
+            opacity: 1,
+        },
+        exit: {
+            zIndex: 0,
+            x: -1920,
+            opacity: 0,
+        },
+    },
+    left: {
+        enter: {
+            zindex: 0,
+            x: -1920,
+            opacity: 0,
+        },
+        center: {
+            zIndex: 1,
+            x: 0,
+            opacity: 1,
+        },
+        exit: {
+            zIndex: 0,
+            x: 1920,
+            opacity: 0,
+        },
+    },
 }
 
 export default function ImageSlider({
-	className,
-	imgPaths,
+    className,
+    imgPaths,
 }: {
-	className?: string
-	imgPaths: string[]
+    className?: string
+    imgPaths: string[]
 }) {
-	function reducer(idx: number, action: "next" | "previous"): number {
-		switch (action) {
-			case "next":
-				return (idx + 1) % imgPaths.length
-			case "previous":
-				return idx === 0 ? imgPaths.length - 1 : idx - 1
-		}
-	}
-	const [idx, setIdx] = useReducer(reducer, 0)
+    type Slide = { idx: number; direction: "left" | "right" }
 
-	useEffect(() => {
-		const interval = setInterval(() => setIdx("next"), 1000 * 6)
-		return () => clearInterval(interval)
-	}, [idx])
+    function reducer(state: Slide, action: "next" | "previous"): Slide {
+        switch (action) {
+            case "next":
+                return {
+                    idx: (state.idx + 1) % imgPaths.length,
+                    direction: "right",
+                }
+            case "previous":
+                return {
+                    idx: state.idx === 0 ? imgPaths.length - 1 : state.idx - 1,
+                    direction: "left",
+                }
+        }
+    }
 
-	return (
-		<div className={twMerge("overflow-hidden relative", className)}>
-			<AnimatePresence>
-				<motion.div
-					key={`imgs-slid-${idx}`}
-					variants={variants}
-					className="absolute inset-0"
-					initial="enter"
-					animate="center"
-					exit="exit"
-					transition={{
-						x: { type: "spring", stiffness: 300, damping: 30 },
-						opacity: { duration: 0.2 },
-					}}
-				>
-					<Image
-						src={imgPaths[idx]}
-						className="object-cover h-full w-full"
-						alt={`imgs-slid-${idx}`}
-						width={1920}
-						height={800}
-						priority
-					/>
-				</motion.div>
-			</AnimatePresence>
-			<button
-				className="absolute z-10 top-[calc(50%_-_20px)] left-[10px] w-[48px] h-[48px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
-				onClick={() => setIdx("previous")}
-			></button>
-			<button
-				className="absolute z-10 top-[calc(50%_-_20px)] right-[10px] w-[48px] h-[48px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
-				onClick={() => setIdx("next")}
-			></button>
-		</div>
-	)
+    const [state, dispach] = useReducer(reducer, {
+        idx: 0,
+        direction: "right",
+    })
+
+    useEffect(() => {
+        const interval = setInterval(() => dispach("next"), 1000 * 6)
+        return () => clearInterval(interval)
+    }, [state])
+
+    return (
+        <div className={twMerge("overflow-hidden relative", className)}>
+            <AnimatePresence>
+                <motion.div
+                    key={`imgs-slid-${state.idx}`}
+                    variants={variants[state.direction]}
+                    className="absolute inset-0"
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                    transition={{
+                        x: { type: "spring", stiffness: 300, damping: 30 },
+                        opacity: { duration: 0.2 },
+                    }}
+                >
+                    <Image
+                        src={imgPaths[state.idx]}
+                        className="object-cover h-full w-full"
+                        alt={`imgs-slid-${state.idx}`}
+                        width={1920}
+                        height={800}
+                        priority
+                    />
+                </motion.div>
+            </AnimatePresence>
+            <button
+                className="absolute z-10 top-[calc(50%_-_20px)] left-[10px] w-[48px] h-[48px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
+                onClick={() => dispach("previous")}
+            ></button>
+            <button
+                className="absolute z-10 top-[calc(50%_-_20px)] right-[10px] w-[48px] h-[48px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
+                onClick={() => dispach("next")}
+            ></button>
+        </div>
+    )
 }
