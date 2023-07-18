1caf09ddef55bc25916098fd38d30fc69d9dc972
bieniucieniu <mikibien@outlook.com>
2023-07-18 14:37:36 +0200

    create component for sicial media link

---
+++ components/SocialLinks.tsx
@@ -0,0 +1,40 @@
+import { StringToBoolean } from "class-variance-authority/dist/types"
+import Image from "next/image"
+import Link from "next/link"
+import { twMerge } from "tailwind-merge"
+
+export default function SocialLinks({
+    className,
+    ...props
+}: React.HTMLProps<HTMLDivElement>) {
+    return (
+        <div className={twMerge("flex flex-row gap-8 px-8")} {...props}>
+            <Link
+                href="https://www.instagram.com/braciabien/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    src="/instagram.svg"
+                    className="m-auto object-contain h-[100px]"
+                    alt="instagram"
+                    width={50}
+                    height={50}
+                ></Image>
+            </Link>
+            <Link
+                href="https://www.facebook.com/BraciaBien/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    src="/facebook.svg"
+                    className="m-auto object-contain h-[100px]"
+                    alt="facebook"
+                    width={50}
+                    height={50}
+                ></Image>
+            </Link>
+        </div>
+    )
+}
