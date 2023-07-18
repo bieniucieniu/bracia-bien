135df392b66b417c051d945ebb52376fadab5113
bieniucieniu <mikibien@outlook.com>
2023-07-18 18:44:28 +0200

    update config

--- app/page.tsx
+++ app/page.tsx
@@ -7,128 +7,125 @@ import Link from "next/link"
 import SocialLinks from "@/components/SocialLinks"
 
 const imgPaths: string[] = [
-    "/images/fotob-33.jpg",
-    "/images/fotob-39.jpg",
-    "/images/fotob-40.jpg",
+  "/images/fotob-33.jpg",
+  "/images/fotob-39.jpg",
+  "/images/fotob-40.jpg",
 ]
 
 const navData: Parameters<typeof Navbar>[0] = {
-    items: [
-        {
-            trigger: "Ona",
-            content: [
-                { href: "", children: "bielzna " },
-                { href: "", children: "rajstopy" },
-                { href: "", children: "skarpetki" },
-                { href: "", children: "pizamy" },
-            ],
-        },
-        {
-            trigger: "On",
-            content: [
-                { href: "", children: "bielzna " },
-                { href: "", children: "skarpetki" },
-                { href: "", children: "pizamy" },
-            ],
-        },
-        {
-            trigger: "dzieci",
-            content: [
-                { href: "", children: "bielzna " },
-                { href: "", children: "skarpetki" },
-                { href: "", children: "pizamy" },
-            ],
-        },
-        { trigger: "inne", href: "" },
-    ],
+  items: [
+    {
+      trigger: "Ona",
+      content: [
+        { href: "", children: "bielzna " },
+        { href: "", children: "rajstopy" },
+        { href: "", children: "skarpetki" },
+        { href: "", children: "pizamy" },
+      ],
+    },
+    {
+      trigger: "On",
+      content: [
+        { href: "", children: "bielzna " },
+        { href: "", children: "skarpetki" },
+        { href: "", children: "pizamy" },
+      ],
+    },
+    {
+      trigger: "dzieci",
+      content: [
+        { href: "", children: "bielzna " },
+        { href: "", children: "skarpetki" },
+        { href: "", children: "pizamy" },
+      ],
+    },
+    { trigger: "inne", href: "" },
+  ],
 }
 
 const cardsData: Parameters<typeof InfoCards>[0] = {
-    cards: [{ className: "w-full" }],
+  cards: [{ className: "w-full" }],
 }
 
 const contactsData: Parameters<typeof InfoTile>[0] = {
-    title: (
-        <>
-            Skontaktuj się
-            <br /> z nami
-        </>
-    ),
-    data: [
-        { name: "+48 502 896 299", href: "tel:+48502896299" },
-        {
-            name: "braciabien@gmail.com",
-            href: "mailto:braciabien@gmail.com",
-        },
-        {
-            name: "Stawiszyńska 125, 62-800 Kalisz",
-            href: "https://goo.gl/maps/BfMbTwFQTeVjVR717",
-            newTab: true,
-        },
-    ],
+  title: (
+    <>
+      Skontaktuj się
+      <br /> z nami
+    </>
+  ),
+  data: [
+    { name: "+48 502 896 299", href: "tel:+48502896299" },
+    {
+      name: "braciabien@gmail.com",
+      href: "mailto:braciabien@gmail.com",
+    },
+    {
+      name: "Stawiszyńska 125, 62-800 Kalisz",
+      href: "https://goo.gl/maps/BfMbTwFQTeVjVR717",
+      newTab: true,
+    },
+  ],
 }
 const infoData: Parameters<typeof InfoTile>[0] = {
-    title: "informacje",
-    className: "bg-blue-400",
-    classNameMotion: "bg-red-500",
-    data: [
-        {
-            name: "Polityka prywatnosci",
-            href: "/pdf/polityka_prywatnosci.pdf",
-            rel: "noopener noreferrer",
-        },
-        {
-            name: "Platnosc",
-            href: "/pdf/platnosc.pdf",
-            rel: "noopener noreferrer",
-        },
-        {
-            name: "regulamin sklepu",
-            href: "/pdf/regulamin_sklepu.pdf",
-            rel: "noopener noreferrer",
-        },
-        {
-            name: "wysylka",
-            href: "/pdf/wysylka.pdf",
-            rel: "noopener noreferrer",
-        },
-    ],
+  title: "informacje",
+  className: "bg-blue-400",
+  classNameMotion: "bg-red-500",
+  data: [
+    {
+      name: "Polityka prywatnosci",
+      href: "/pdf/polityka_prywatnosci.pdf",
+      rel: "noopener noreferrer",
+    },
+    {
+      name: "Platnosc",
+      href: "/pdf/platnosc.pdf",
+      rel: "noopener noreferrer",
+    },
+    {
+      name: "regulamin sklepu",
+      href: "/pdf/regulamin_sklepu.pdf",
+      rel: "noopener noreferrer",
+    },
+    {
+      name: "wysylka",
+      href: "/pdf/wysylka.pdf",
+      rel: "noopener noreferrer",
+    },
+  ],
 }
 
 export default function Home() {
-    return (
-        <main
-            style={{ scrollbarGutter: "stable" }}
-            className="snap-y snap-proximity overflow-auto h-screen"
-        >
-            <div className="flex flex-col min-h-screen relative bg-yellow-200 snap-center">
-                <nav className="bg-white grid grid-cols-3 grid-rows-[100px_1fr]">
-                    <SocialLinks />
-                    <Link href="/">
-                        <Image
-                            src="/logo.png"
-                            className="m-auto object-contain h-[100px]"
-                            alt="logo"
-                            width={638}
-                            height={189}
-                        ></Image>
-                    </Link>
-                    <Navbar
-                        {...navData}
-                        className="row-start-2 row-end-2 col-span-3"
-                    />
-                </nav>
-            </div>
-            <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 snap-center">
-                <ImageSlider className="flex-1" imgPaths={imgPaths} />
-                <div className="bg-red-600 flex p-10">
-                    <InfoCards {...cardsData} />
-                </div>
-            </div>
-            <footer className="min-h-screen grid grid-cols-1 xl:grid-cols-3 snap-center">
-                <InfoTile className={"col-span-2"} {...contactsData} />
-                <InfoTile {...infoData} />
-            </footer>
-        </main>
-    )
+  return (
+    <main
+      style={{ scrollbarGutter: "stable" }}
+      className="snap-y snap-proximity overflow-auto h-screen"
+    >
+      <div className="flex flex-col min-h-screen relative bg-yellow-200 snap-center">
+        <nav className="bg-white grid grid-cols-3 grid-rows-[100px_1fr]">
+          <SocialLinks />
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              className="m-auto object-contain h-[100px]"
+              alt="logo"
+              width={638}
+              height={189}
+            ></Image>
+          </Link>
+          <Navbar {...navData} className="row-start-2 row-end-2 col-span-3" />
+        </nav>
+      </div>
+      <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 snap-center">
+        <ImageSlider className="flex-1" imgPaths={imgPaths} />
+        <div className="bg-red-600 flex p-10">
+          <InfoCards {...cardsData} />
+        </div>
+      </div>
+      <footer className="min-h-screen grid grid-cols-1 xl:grid-cols-3 snap-center">
+        <InfoTile className={"col-span-2"} {...contactsData} />
+        <InfoTile {...infoData} />
+      </footer>
+    </main>
+  )
 }
--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -4,113 +4,113 @@ import { AnimatePresence, motion } from "framer-motion"
 import Image from "next/image"
 import { twMerge } from "tailwind-merge"
 const variants = {
-    enter: (direction: "left" | "right") => ({
-        zIndex: 0,
-        x: { left: -1920, right: 1920 }[direction],
-        opacity: 0,
-    }),
-    center: {
-        zIndex: 1,
-        x: 0,
-        opacity: 1,
-    },
-    exit: (direction: "left" | "right") => ({
-        zindex: 0,
-        x: { left: 1920, right: -1920 }[direction],
-        opacity: 0,
-    }),
+  enter: (direction: "left" | "right") => ({
+    zIndex: 0,
+    x: { left: -1920, right: 1920 }[direction],
+    opacity: 0,
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: "left" | "right") => ({
+    zindex: 0,
+    x: { left: 1920, right: -1920 }[direction],
+    opacity: 0,
+  }),
 }
 
 export default function ImageSlider({
-    className,
-    imgPaths,
-    full,
+  className,
+  imgPaths,
+  full,
 }: {
-    className?: string
-    imgPaths: string[]
-    full?: boolean
+  className?: string
+  imgPaths: string[]
+  full?: boolean
 }) {
-    type Slide = { idx: number; direction: "left" | "right" }
+  type Slide = { idx: number; direction: "left" | "right" }
 
-    function reducer(state: Slide, action: "next" | "previous"): Slide {
-        switch (action) {
-            case "next":
-                return {
-                    idx: (state.idx + 1) % imgPaths.length,
-                    direction: "right",
-                }
-            case "previous":
-                return {
-                    idx: state.idx === 0 ? imgPaths.length - 1 : state.idx - 1,
-                    direction: "left",
-                }
+  function reducer(state: Slide, action: "next" | "previous"): Slide {
+    switch (action) {
+      case "next":
+        return {
+          idx: (state.idx + 1) % imgPaths.length,
+          direction: "right",
+        }
+      case "previous":
+        return {
+          idx: state.idx === 0 ? imgPaths.length - 1 : state.idx - 1,
+          direction: "left",
         }
     }
+  }
 
-    const [state, dispach] = useReducer(reducer, {
-        idx: 0,
-        direction: "right",
-    })
+  const [state, dispach] = useReducer(reducer, {
+    idx: 0,
+    direction: "right",
+  })
 
-    useEffect(() => {
-        const interval = setInterval(() => dispach("next"), 1000 * 6)
-        return () => clearInterval(interval)
-    }, [state])
+  useEffect(() => {
+    const interval = setInterval(() => dispach("next"), 1000 * 6)
+    return () => clearInterval(interval)
+  }, [state])
 
-    return (
-        <div className={twMerge("overflow-hidden relative", className)}>
-            <AnimatePresence custom={state.direction}>
-                <motion.div
-                    custom={state.direction}
-                    key={`imgs-slid-${state.idx}`}
-                    className={`absolute inset-0 flex  items-center transition-all ${
-                        full ? "" : "p-2"
-                    } `}
-                    variants={variants}
-                    initial="enter"
-                    animate="center"
-                    exit="exit"
-                    transition={{
-                        x: { type: "spring", stiffness: 300, damping: 30 },
-                        opacity: { duration: 0.2 },
-                    }}
-                >
-                    <Image
-                        src={imgPaths[state.idx]}
-                        className={`object-contain transition-all h-fit w-fit flex-1 ${
-                            full ? "" : "rounded-xl"
-                        }`}
-                        alt={`imgs-slid-${state.idx}`}
-                        width={1920}
-                        height={800}
-                        priority
-                    />
-                </motion.div>
-            </AnimatePresence>
-            <button
-                className="absolute z-10 top-[calc(50%_-_20px)] left-[15px] w-[40px] h-[40px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
-                onClick={() => dispach("previous")}
-            >
-                <Image
-                    src="/angle-left.svg"
-                    alt="left arrow"
-                    className="object-contain absolute top-[calc(50%_-_15px)] left-[3px]"
-                    width={30}
-                    height={30}
-                ></Image>
-            </button>
-            <button
-                className="absolute z-10 top-[calc(50%_-_20px)] right-[15px] w-[40px] h-[40px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
-                onClick={() => dispach("next")}
-            >
-                <Image
-                    src="/angle-right.svg"
-                    alt="left right"
-                    className="object-contain absolute top-[calc(50%_-_15px)] right-[3px]"
-                    width={30}
-                    height={30}
-                ></Image>
-            </button>
-        </div>
-    )
+  return (
+    <div className={twMerge("overflow-hidden relative", className)}>
+      <AnimatePresence custom={state.direction}>
+        <motion.div
+          custom={state.direction}
+          key={`imgs-slid-${state.idx}`}
+          className={`absolute inset-0 flex  items-center transition-all ${
+            full ? "" : "p-2"
+          } `}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+        >
+          <Image
+            src={imgPaths[state.idx]}
+            className={`object-contain transition-all h-fit w-fit flex-1 ${
+              full ? "" : "rounded-xl"
+            }`}
+            alt={`imgs-slid-${state.idx}`}
+            width={1920}
+            height={800}
+            priority
+          />
+        </motion.div>
+      </AnimatePresence>
+      <button
+        className="absolute z-10 top-[calc(50%_-_20px)] left-[15px] w-[40px] h-[40px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
+        onClick={() => dispach("previous")}
+      >
+        <Image
+          src="/angle-left.svg"
+          alt="left arrow"
+          className="object-contain absolute top-[calc(50%_-_15px)] left-[3px]"
+          width={30}
+          height={30}
+        ></Image>
+      </button>
+      <button
+        className="absolute z-10 top-[calc(50%_-_20px)] right-[15px] w-[40px] h-[40px] rounded-full bg-white opacity-50 hover:opacity-100 transition-all"
+        onClick={() => dispach("next")}
+      >
+        <Image
+          src="/angle-right.svg"
+          alt="left right"
+          className="object-contain absolute top-[calc(50%_-_15px)] right-[3px]"
+          width={30}
+          height={30}
+        ></Image>
+      </button>
+    </div>
+  )
 }
