d482236784603ab8408a0e1ee73c438cf1da2618
bieniucieniu <mikibien@outlook.com>
2023-07-24 16:45:56 +0200

    basic data representation

--- app/admin/upload/page.tsx
+++ app/admin/upload/page.tsx
@@ -1,13 +1,27 @@
 import AuthButton from "@/components/Auth"
-import UploadButton from "@/components/UploadButton"
+import UploadButton from "@/components/Upload"
+import { listFiles } from "@/utils/uploadthing"
 import { utapi } from "uploadthing/server"
 
 export default async function Upload() {
-	// const files = await utapi.listFiles()
-	return (
-		<main className="pt-20">
-			<AuthButton />
-			<UploadButton />
-		</main>
-	)
+  const files = await listFiles()
+  const paths = await utapi.getFileUrls(files.map((e) => e.key))
+  return (
+    <main className="pt-20">
+      <AuthButton />
+      <UploadButton />
+      {files.map((e) => (
+        <li>
+          <p>{e.id}</p>
+          <p>{e.key}</p>
+        </li>
+      ))}
+      {paths.map((e) => (
+        <li>
+          <p>{e.url}</p>
+          <p>{e.key}</p>
+        </li>
+      ))}
+    </main>
+  )
 }
