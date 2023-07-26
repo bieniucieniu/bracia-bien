1ca81c180755b53b0732aef29ff917d851afb193
bieniucieniu <mikibien@outlook.com>
2023-07-26 14:07:21 +0200

    formating

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,6 +1,6 @@
 import {
-  createUploadthing,
-  type FileRouter as RouterUpload,
+	createUploadthing,
+	type FileRouter as RouterUpload,
 } from "uploadthing/next"
 import { getServerSession } from "next-auth"
 import authOptions from "./auth"
@@ -8,32 +8,55 @@ import authOptions from "./auth"
 const f = createUploadthing()
 
 const auth = async (_req: Request) => {
-  const session = await getServerSession(authOptions)
-  if (!session) return
-  switch (session.user?.email) {
-    case "bienmikolaj@gmail.com":
-    case "braciabien@gmail.com":
-      return { id: session.user.email }
+	const session = await getServerSession(authOptions)
+	if (!session) return
+	switch (session.user?.email) {
+		case "bienmikolaj@gmail.com":
+		case "braciabien@gmail.com":
+			return { id: session.user.email }
 
-    default:
-      return undefined
-  }
+		default:
+			return undefined
+	}
 }
 
 export const fileRouter = {
-  image: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    .middleware(async ({ req }) => {
-      const user = await auth(req)
+	image: f({ image: { maxFileSize: "1MB", maxFileCount: 999999 } })
+		.middleware(async ({ req }) => {
+			const user = await auth(req)
 
-      if (!user) throw new Error("Unauthorized")
+			if (!user) throw new Error("Unauthorized")
 
-      return { userId: user.id }
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.userId)
+			return { userId: user.id }
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			console.log("Upload complete for userId:", metadata.userId)
+			console.log("file url", file.url)
+		}),
+	current: f({ image: { maxFileSize: "1MB", maxFileCount: 10 } })
+		.middleware(async ({ req }) => {
+			const user = await auth(req)
 
-      console.log("file url", file.url)
-    }),
+			if (!user) throw new Error("Unauthorized")
+
+			return { userId: user.id }
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			console.log("Upload complete for userId:", metadata.userId)
+			console.log("file url", file.url)
+		}),
+	main: f({ image: { maxFileSize: "1MB", maxFileCount: 10 } })
+		.middleware(async ({ req }) => {
+			const user = await auth(req)
+
+			if (!user) throw new Error("Unauthorized")
+
+			return { userId: user.id }
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			console.log("Upload complete for userId:", metadata.userId)
+			console.log("file url", file.url)
+		}),
 } satisfies RouterUpload
 
 export type FileRouter = typeof fileRouter
