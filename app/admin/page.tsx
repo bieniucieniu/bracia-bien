89a219bf9795f7a8cc1ff54510a9538fc5b84b77
bieniucieniu <mikibien@outlook.com>
2023-07-26 14:07:38 +0200

    add edge config

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,13 +4,13 @@ import { useSession } from "next-auth/react"
 import Link from "next/link"
 
 export default async function Admin() {
-  const { data } = useSession()
-  return (
-    <div className="pt-10">
-      <p>
-        <AuthButton />
-        {data ? <Link href="admin/upload"></Link> : "not loged in"}
-      </p>
-    </div>
-  )
+	const { data } = useSession()
+	return (
+		<div className="pt-10">
+			<p>
+				<AuthButton />
+				{data ? <Link href="admin/upload"></Link> : "not loged in"}
+			</p>
+		</div>
+	)
 }
