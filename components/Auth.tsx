35f300936a23f5c4b8757f7d89a8c64c24ea4292
bieniucieniu <mikibien@outlook.com>
2023-07-27 20:02:39 +0200

    auth styling

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,20 +4,20 @@ import { Button } from "@/components/ui/button"
 
 export default function AuthButton() {
   const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user?.name} <br />
-        <Button onClick={() => signOut()}>Sign out</Button>
-      </>
-    )
-  }
+
   return (
-    <>
-      <Button onClick={() => signIn()}>
-        Sign in
-        <span className="text-gray-500">Not signed in</span>
-      </Button>
-    </>
+    <div className="flex flex-row items-center gap-2 w-fit px-2 py-1 rounded-md text-sm font-medium transition-colors border border-input bg-background shadow-sm">
+      {session ? (
+        <>
+          <span>Signed in as {session.user?.name}</span>
+          <Button onClick={() => signOut()}>Sign out</Button>
+        </>
+      ) : (
+        <>
+          <span>Not signed in</span>
+          <Button onClick={() => signIn()}>Sign in</Button>
+        </>
+      )}
+    </div>
   )
 }
