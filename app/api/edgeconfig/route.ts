0885f886ef630c37264a3cc34778d181580949f2
bieniucieniu <mikibien@outlook.com>
2023-07-28 13:00:35 +0200

    extend check

--- app/api/edgeconfig/route.ts
+++ app/api/edgeconfig/route.ts
@@ -3,8 +3,10 @@ import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
+  const allowed = ["bienmikolaj@gmail.com", "braciabien@gmail.com"]
   const session = await getServerSession(authOptions)
-  if (!session) return NextResponse.json("not authenticated")
+  if (!session || !session.user?.email || !allowed.includes(session.user.email))
+    return NextResponse.json("Unauthorized", { status: 401 })
 
   return NextResponse.json(session.user)
 }
