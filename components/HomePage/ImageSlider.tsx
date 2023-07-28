41c0c88aecd346859fa6c047d309e3091d2a09e3
bieniucieniu <mikibien@outlook.com>
2023-07-28 19:04:38 +0200

    change to img url prop

--- components/HomePage/ImageSlider.tsx
+++ components/HomePage/ImageSlider.tsx
@@ -1,39 +1,15 @@
 "use client"
 import Image from "next/image"
-import { useMainLayoutContext } from "@/components/MainLayout/MainLayoutContext"
 import Slider from "../Slider"
 
-export function ImageSliderMain() {
-  const { imgs } = useMainLayoutContext()
+export function ImageSlider({ urls }: { urls: string[] }) {
   return (
     <Slider
       className="m-2"
       renderer={(i) => (
-        <Image
-          alt={`image-${i}`}
-          src={imgs.main[i]}
-          width={1920}
-          height={800}
-        />
+        <Image alt={`image-${i}`} src={urls[i]} width={1920} height={800} />
       )}
-      length={imgs.main.length}
-    />
-  )
-}
-export function ImageSliderCurrent() {
-  const { imgs } = useMainLayoutContext()
-  return (
-    <Slider
-      className="m-2"
-      renderer={(i) => (
-        <Image
-          alt={`image-${i}`}
-          src={imgs.current[i]}
-          width={1920}
-          height={800}
-        />
-      )}
-      length={imgs.current.length}
+      length={urls.length}
     />
   )
 }
