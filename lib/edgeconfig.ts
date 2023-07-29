41c6c98ca97a1c1c45fb24d0a495598f5f815998
bieniucieniu <mikibien@outlook.com>
2023-07-29 18:30:39 +0200

    remove session condition

--- lib/edgeconfig.ts
+++ lib/edgeconfig.ts
@@ -1,5 +1,3 @@
-import { useSession } from "next-auth/react"
-
 export type edgeConfigType = {
   mainImgKeys?: string[]
   currentImgKeys?: string[]
@@ -8,9 +6,6 @@ export async function setConfig(
   newConfig: edgeConfigType,
   onComplete?: (res: Response) => void,
 ) {
-  const { data: session } = useSession()
-  if (!session) return
-
   const res = await fetch("/api/edgeconfig", {
     method: "POST",
     body: JSON.stringify({
