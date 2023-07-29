83abbeb61b263216ba3edec6cc53fe94b132fab1
bieniucieniu <mikibien@outlook.com>
2023-07-29 18:16:44 +0200

    add set function

--- lib/edgeconfig.ts
+++ lib/edgeconfig.ts
@@ -1,15 +1,22 @@
+import { useSession } from "next-auth/react"
+
 export type edgeConfigType = {
   mainImgKeys?: string[]
   currentImgKeys?: string[]
 }
+export async function setConfig(
+  newConfig: edgeConfigType,
+  onComplete?: (res: Response) => void,
+) {
+  const { data: session } = useSession()
+  if (!session) return
 
-export type edgePOST = {
-  mainImgKeys: {
-    delete: string[]
-    add: string[]
-  }
-  currentImgKeys?: {
-    delete: string[]
-    add: string[]
-  }
+  const res = await fetch("/api/edgeconfig", {
+    method: "POST",
+    body: JSON.stringify({
+      config: newConfig,
+    }),
+  })
+
+  onComplete && onComplete(res)
 }
