ef82bd09265a95e21bdaf641eb70c2c54f6b9ac3
bieniucieniu <mikibien@outlook.com>
2023-08-01 15:51:22 +0200

    add space replacer

--- components/admin/Upload.tsx
+++ components/admin/Upload.tsx
@@ -26,7 +26,18 @@ export function UploadZone({
   const [files, setFiles] = useState<File[]>([])
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
-      setFiles([...files, ...acceptedFiles])
+      const f = acceptedFiles.map((e) => {
+        console.log(e)
+        if (e.name.includes(" ")) {
+          // const blob = e.slice(0, e.size)
+          return new File([e], e.name.replaceAll(" ", "_"), {
+            type: e.type,
+          })
+        }
+        return e
+      })
+
+      setFiles([...files, ...f])
     },
     [files],
   )
@@ -61,7 +72,7 @@ export function UploadZone({
             width,
             height,
           },
-          className: `border-2  border-dashed border-black rounded-xl p-5 w-fill h-fill`,
+          className: `border-2 border-dashed border-black rounded-xl p-5 w-fill h-fill`,
         })}
       >
         <input {...getInputProps({ disabled: isUploading })} />
