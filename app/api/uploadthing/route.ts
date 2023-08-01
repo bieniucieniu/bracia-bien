ae6e4172248315794b21983ef3bc83fb09726fe4
bieniucieniu <mikibien@outlook.com>
2023-08-01 22:15:13 +0200

    add delete reqest response

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,34 @@
 import { createNextRouteHandler } from "uploadthing/next"
 
 import { fileRouter } from "@/lib/uploadthing"
+import { getServerSession } from "next-auth"
+import authOptions from "@/lib/auth"
+import { NextResponse } from "next/server"
+import { utapi } from "uploadthing/server"
+import { z } from "zod"
 
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
-	router: fileRouter,
+  router: fileRouter,
 })
+
+const deleteSchema = z.string().array()
+
+export async function DELETE(req: Request) {
+  const allowed = ["bienmikolaj@gmail.com", "braciabien@gmail.com"]
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user?.email || !allowed.includes(session.user.email))
+    return NextResponse.json("Unauthorized", { status: 401 })
+
+  const data: z.infer<typeof deleteSchema> = await req.json()
+
+  if (!deleteSchema.safeParse(data).success)
+    return NextResponse.json(
+      { message: "invalid data", data: data },
+      { status: 401 },
+    )
+
+  const res = await utapi.deleteFiles(data)
+
+  return NextResponse.json(res)
+}
