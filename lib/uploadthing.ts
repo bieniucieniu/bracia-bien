9e80db24b0d2b7902ac38d8ecf78d818839e2fbb
bieniucieniu <mikibien@outlook.com>
2023-08-01 21:45:56 +0200

    change use validation

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -8,16 +8,16 @@ import authOptions from "./auth"
 const f = createUploadthing()
 
 const auth = async (_req: Request) => {
-  const session = await getServerSession(authOptions)
-  if (!session?.user) return
-  switch (session.user.email) {
-    case "bienmikolaj@gmail.com":
-    case "braciabien@gmail.com":
-      return { id: session.user.email }
+  const allowedEmails = ["bienmikolaj@gmail.com", "braciabien@gmail.com"]
 
-    default:
-      return undefined
-  }
+  const session = await getServerSession(authOptions)
+  if (
+    !session ||
+    !session.user?.email ||
+    !allowedEmails.includes(session.user.email)
+  )
+    return undefined
+  return { id: session.user.email }
 }
 
 export const fileRouter = {
