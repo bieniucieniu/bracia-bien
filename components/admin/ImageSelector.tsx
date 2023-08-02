b0c86f5faadec985085986e050f3e641717c0f34
bieniucieniu <mikibien@outlook.com>
2023-08-02 14:40:52 +0200

    add select and delete

--- components/admin/ImageSelector.tsx
+++ components/admin/ImageSelector.tsx
@@ -26,7 +26,9 @@ import {
 import Link from "next/link"
 import { setConfig } from "@/lib/edgeconfig"
 import { z } from "zod"
+import { deleteFiles } from "@/utils/uploadthing"
 
+import { useRouter } from "next/navigation"
 export default function ImageSelesctor({
   imgsData,
   config,
@@ -36,8 +38,10 @@ export default function ImageSelesctor({
 }) {
   const [main, setMain] = useState<string[]>(config.mainImgKeys ?? [])
   const [current, setCurrent] = useState<string[]>(config.currentImgKeys ?? [])
+  const [toDelete, setToDelete] = useState<string[]>([])
+  const router = useRouter()
 
-  const addDeleteMain = useCallback(
+  const addRemoveMain = useCallback(
     (key: string) => {
       if (main.includes(key)) {
         const newItems = main.filter((k) => k !== key)
@@ -50,7 +54,7 @@ export default function ImageSelesctor({
     },
     [main],
   )
-  const addDeleteCurrent = useCallback(
+  const addRemoveCurrent = useCallback(
     (key: string) => {
       if (current.includes(key)) {
         const newItems = current.filter((k) => k !== key)
@@ -64,15 +68,34 @@ export default function ImageSelesctor({
     [current],
   )
 
+  const addRemoveToDelete = useCallback(
+    (key: string) => {
+      if (toDelete.includes(key)) {
+        const newItems = toDelete.filter((k) => k !== key)
+        setToDelete(newItems)
+      } else {
+        toDelete.push(key)
+        setToDelete([...toDelete])
+      }
+    },
+    [toDelete],
+  )
+
   const setSelected = () => {
     setConfig({ mainImgKeys: main, currentImgKeys: current }, async (res) => {
       const body = await res.json()
-      if (body.error) {
-        console.log(body)
+      if (body.error) throw new Error(body.error)
+    })
+  }
+  const deleteSelected = () => {
+    deleteFiles(toDelete, async (res) => {
+      const body = await res.json()
+      if (body.success) {
+        setToDelete([])
+        router.refresh()
       }
     })
   }
-
   return (
     <Card className="flex flex-col w-fit m-auto" id="imgSel">
       <CardHeader>
@@ -88,22 +111,29 @@ export default function ImageSelesctor({
                 src={img.url}
                 width={400}
                 height={300}
-                className="object-contain h-auto"
+                className="object-contain h-auto rounded-t-xl"
               />
-              <section className="flex justify-around">
+              <section className="flex justify-around pb-2 rounded-b-xl shadow-md">
                 <Button
-                  variant={main.includes(img.key) ? "destructive" : "default"}
-                  onClick={() => addDeleteMain(img.key)}
+                  variant={main.includes(img.key) ? "green" : "default"}
+                  onClick={() => addRemoveMain(img.key)}
                 >
                   main
                 </Button>
                 <Button
-                  variant={
-                    current.includes(img.key) ? "destructive" : "default"
-                  }
-                  onClick={() => addDeleteCurrent(img.key)}
+                  variant={current.includes(img.key) ? "green" : "default"}
+                  onClick={() => addRemoveCurrent(img.key)}
                 >
                   current
+                </Button>
+                <Button
+                  variant={
+                    toDelete.includes(img.key) ? "destructive" : "default"
+                  }
+                  onClick={() => addRemoveToDelete(img.key)}
+                  className="outline outline-2 outline-offset-2 outline-red-500"
+                >
+                  to delete
                 </Button>
               </section>
             </li>
@@ -112,6 +142,28 @@ export default function ImageSelesctor({
       </CardContent>
       <CardFooter className="justify-end">
         <div className="sticky bottom-2 flex gap-1 bg-white shadow-sm rounded-xl p-2">
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive" disabled={toDelete.length <= 0}>
+                delete Selected
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Delete {toDelete.length} files?
+                </AlertDialogTitle>
+                <AlertDialogDescription>are you sure</AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={deleteSelected}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+
           <AlertDialog>
             <AlertDialogTrigger asChild>
               <Button>Set Selected</Button>
