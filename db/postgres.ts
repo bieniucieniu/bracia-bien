ae1826b316f2f1e4acbc10c4c5cebdca598ee515
bieniucieniu <mikibien@outlook.com>
2023-08-08 21:43:37 +0200

    add schema and functiong to interact w db w validation

--- db/postgres.ts
+++ db/postgres.ts
@@ -1,51 +1,74 @@
 import { drizzle } from "drizzle-orm/vercel-postgres"
 import { sql } from "@vercel/postgres"
-import { eq, inArray } from "drizzle-orm"
+import { inArray } from "drizzle-orm"
 import { createInsertSchema, createSelectSchema } from "drizzle-zod"
-import { images } from "./schema"
-import type { z } from "zod"
-import { NextResponse } from "next/server"
+import { images, categoryEnum } from "./schema"
+import { z } from "zod"
+import { type NextResponse } from "next/server"
+
+type JsonRes = Parameters<(typeof NextResponse)["json"]>
 
 export const db = drizzle(sql)
 
 export const insertImagesSchema = createInsertSchema(images)
 export const selectImagesSchema = createSelectSchema(images)
 
-export async function getImages(category: "current" | "main" | "" = "") {
-	const selectResult = await db
-		.select()
-		.from(images)
-		.where(eq(images.category, category))
+const uuidArraySchema = z.string().uuid().array()
+const categorySchema = z.enum(categoryEnum.enumValues)
 
-	return selectResult
+export async function getCategoty(
+  category: z.infer<typeof categorySchema>[],
+): Promise<JsonRes> {
+  if (!categorySchema.array().safeParse(categorySchema))
+    return [{ error: "invalid data" }, { status: 400 }]
+
+  const res = await db
+    .select()
+    .from(images)
+    .where(inArray(images.category, category))
+
+  return [{ res }]
 }
 
-export async function addImages(data: z.infer<typeof insertImagesSchema>[]) {
-	if (!insertImagesSchema.safeParse(data))
-		return NextResponse.json({ error: "invalid data" }, { status: 400 })
+export async function addImages(
+  data: z.infer<typeof insertImagesSchema>[],
+): Promise<JsonRes> {
+  if (!insertImagesSchema.safeParse(data))
+    return [{ error: "invalid data" }, { status: 400 }] as JsonRes
 
-	const res = await db.insert(images).values(data)
+  const res = await db.insert(images).values(data).returning()
 
-	return NextResponse.json(res)
+  return [{ res }]
 }
+
+const imagesOmitKey = selectImagesSchema.omit({ key: true })
 
 export async function updateImages({
-	keys,
-	update,
+  keys,
+  update,
 }: {
-	keys: string[]
-	update: {
-		alt?: string
-		category?: z.infer<typeof insertImagesSchema>["category"]
-	}
-}) {
-	if (!insertImagesSchema.omit({ key: true }).safeParse(update))
-		return NextResponse.json({ error: "invalid data" }, { status: 400 })
+  keys: z.infer<typeof uuidArraySchema>
+  update: z.infer<typeof imagesOmitKey>
+}): Promise<JsonRes> {
+  if (!imagesOmitKey.safeParse(update) || !uuidArraySchema.safeParse(keys))
+    return [{ error: "invalid data" }, { status: 400 }] as JsonRes
 
-	const res = await db
-		.update(images)
-		.set(update)
-		.where(inArray(images.key, keys))
+  const res = await db
+    .update(images)
+    .set(update)
+    .where(inArray(images.key, keys))
+    .returning()
 
-	return NextResponse.json(res)
+  return [{ res }]
+}
+
+export async function deleteImages(
+  keys: z.infer<typeof uuidArraySchema>,
+): Promise<JsonRes> {
+  if (!uuidArraySchema.safeParse(keys))
+    return [{ error: "invalid data" }, { status: 400 }] as JsonRes
+
+  const res = await db.delete(images).where(inArray(images.key, keys))
+
+  return [{ res }]
 }
--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,9 @@
 import { pgEnum, pgTable, uuid, varchar } from "drizzle-orm/pg-core"
 
-const categoryEnum = pgEnum("category", ["current", "main", ""])
+export const categoryEnum = pgEnum("category", ["current", "main", ""])
 
 export const images = pgTable("images", {
-	key: uuid("id").primaryKey().unique(),
-	category: categoryEnum("category").default(""),
-	alt: varchar("alt").default(""),
+  key: uuid("id").primaryKey().unique(),
+  category: categoryEnum("category").default(""),
+  alt: varchar("alt").default(""),
 })
