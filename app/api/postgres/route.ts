730da84b9aebd717495a0968a1beb9d78645afe2
bieniucieniu <mikibien@outlook.com>
2023-08-08 21:44:15 +0200

    in progress

--- app/api/postgres/route.ts
+++ app/api/postgres/route.ts
@@ -1,3 +1,29 @@
-export function GET() { }
-export function POST() { }
-export function PATCH() { }
+import { getAuth } from "@/lib/auth"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+  const session = await getAuth()
+  if (!session)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
+
+  const data = await req.json()
+
+  return NextResponse.json({})
+}
+export async function PATCH(req: Request) {
+  const session = await getAuth()
+  if (!session)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
+
+  const data = await req.json()
+
+  const res = []
+
+  if (data.update) {
+  }
+
+  if (data.delete) {
+  }
+
+  return NextResponse.json({})
+}
