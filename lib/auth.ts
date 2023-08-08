7472bcafb564a3bfe9af1135eed9c989e81de781
bieniucieniu <mikibien@outlook.com>
2023-08-08 21:44:07 +0200

    create server auth wraper

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,23 @@
-import type { NextAuthOptions } from "next-auth"
+import { getServerSession, Session, type NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { type NextResponse } from "next/server"
+
+const allowed = ["bienmikolaj@gmail.com", "braciabien@gmail.com"]
 
 const authOptions: NextAuthOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		}),
-	],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
 }
 export default authOptions
+
+export async function getAuth(): Promise<undefined | Session> {
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user?.email || !allowed.includes(session.user.email))
+    return undefined
+
+  return session
+}
