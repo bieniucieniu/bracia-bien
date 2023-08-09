a5b5c23ae9652f34778769d851acae2170450681
bieniucieniu <mikibien@outlook.com>
2023-08-09 15:51:05 +0200

    remove unused func

--- utils/uploadthing.ts
+++ utils/uploadthing.ts
@@ -1,19 +1,11 @@
-import { deleteSchema } from "@/lib/uploadthing"
-import { generateUploadThingURL } from "@uploadthing/shared"
-function getApiKeyOrThrow() {
-  if (!process.env.UPLOADTHING_SECRET)
-    throw new Error("Missing UPLOADTHING_SECRET env variable.")
-  return process.env.UPLOADTHING_SECRET
-}
-
 export async function deleteFiles(
-  items: string[],
-  onCompleat?: (res: Response) => void,
+	items: string[],
+	onCompleat?: (res: Response) => void,
 ) {
-  const res = await fetch("/api/uploadthing", {
-    method: "PATCH",
-    body: JSON.stringify({ items }),
-  })
+	const res = await fetch("/api/uploadthing", {
+		method: "PATCH",
+		body: JSON.stringify({ items }),
+	})
 
-  onCompleat && onCompleat(res)
+	onCompleat && onCompleat(res)
 }
