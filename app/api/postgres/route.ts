85ae8c3652201d234e00e45a5835477bc92048a1
bieniucieniu <mikibien@outlook.com>
2023-08-09 15:27:57 +0200

    full error handling

--- app/api/postgres/route.ts
+++ app/api/postgres/route.ts
@@ -1,29 +1,42 @@
+import { addImages, deleteImages, updateImages } from "@/db/postgres"
 import { getAuth } from "@/lib/auth"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
-  const session = await getAuth()
-  if (!session)
-    return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
+	const session = await getAuth()
+	if (!session)
+		return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
 
-  const data = await req.json()
+	const data = await req.json()
+	if (data.images) {
+		const added = await addImages(data.images)
+		return NextResponse.json({ added })
+	}
 
-  return NextResponse.json({})
+	NextResponse.json({ message: "no data provaided" })
 }
 export async function PATCH(req: Request) {
-  const session = await getAuth()
-  if (!session)
-    return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
+	const session = await getAuth()
+	if (!session)
+		return NextResponse.json({ error: "Unauthorized" }, { status: 403 })
 
-  const data = await req.json()
+	const data = await req.json()
 
-  const res = []
+	const res: { deleted?: any; updated?: any } = {}
 
-  if (data.update) {
-  }
+	if (data.updateImages) {
+		const updated = await updateImages(data.update)
+		res.updated = updated
+	}
 
-  if (data.delete) {
-  }
+	if (data.deleteImages) {
+		const deleted = await deleteImages(data.delete)
+		res.deleted = deleted
+	}
 
-  return NextResponse.json({})
+	if (res.deleted && res.updated) {
+		return NextResponse.json({ massage: "no data provaided" })
+	}
+
+	return NextResponse.json(res)
 }
