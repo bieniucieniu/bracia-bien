8f3ab34752233debe338fac5505bad19e3e3f79f
bieniucieniu <mikibien@outlook.com>
2023-08-11 15:40:30 +0200

    rename column

--- db/schema.ts
+++ db/schema.ts
@@ -3,7 +3,7 @@ import { pgEnum, pgTable, varchar } from "drizzle-orm/pg-core"
 export const categorieEnum = pgEnum("category", ["current", "main", ""])
 
 export const images = pgTable("images", {
-  key: varchar("id").primaryKey().unique(),
+  key: varchar("key").primaryKey().unique(),
   categorie: categorieEnum("categorie").default(""),
   alt: varchar("alt").default(""),
 })
