4f9ea16a94c0ac817ef0abe8388d9d8dd6e6dd18
bieniucieniu <mikibien@outlook.com>
2023-09-06 11:10:30 +0200

    change admin language to Polish

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
+import type { ClassValue } from "clsx"
+import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const toPlObj = {
+  gallery: "galleria",
+  main: "główne",
+  current: "aktualne",
+  else: "inne",
+}
+
+export function toPl(value: keyof typeof toPlObj) {
+  return toPlObj[value]
 }
