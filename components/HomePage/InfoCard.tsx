0f4ea3d3441cf26f86ad2bbc92769dc30c547be3
bieniucieniu <mikibien@outlook.com>
2023-09-20 15:15:35 +0200

    rename file

--- components/HomePage/InfoCard.tsx
+++ components/HomePage/InfoCard.tsx
@@ -1,14 +1,71 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+"use client"
+import Link from "next/link"
+import Slider from "../Slider"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import type { insertCardSchema } from "@/db/infoCard/serverApi"
+import type { z } from "zod"
+import Image from "next/image"
+import { twMerge } from "tailwind-merge"
+import { ScrollArea, ScrollBar } from "../ui/scroll-area"
 
-export default function InfoCard({ className }: { className: string }) {
+export function InfoCards({
+  cards,
+  className,
+}: {
+  cards: (z.infer<typeof insertCardSchema> & { imageUrl?: string })[]
+  className?: string
+}) {
+  if (!cards.length) return null
   return (
-    <Card className={className}>
-      <CardHeader>
-        <CardTitle>Informacje</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-row w-full justify-around gap-3 flex-wrap"></div>
-      </CardContent>
-    </Card>
+    <Slider
+      className={twMerge("m-2", className)}
+      autoSlide={false}
+      length={cards.length}
+      renderer={(i) => {
+        const d = cards[i]
+        return (
+          <Card className="md:mx-16 max-h-[calc(100svh_-_300px)]">
+            <ScrollArea className="m-1 h-[calc(100svh_-_308px)]">
+              <CardHeader>
+                <CardTitle>{d.title}</CardTitle>
+                {d.description ? (
+                  <CardDescription>{d.description}</CardDescription>
+                ) : null}
+              </CardHeader>
+              <CardContent className="flex flex-col gap-y-3 h-full ">
+                <p>{d.content}</p>
+                {d.imageUrl ? (
+                  <Image
+                    className="object-top rounded-xl bg-black w-full h-full"
+                    src={d.imageUrl}
+                    alt={d.imageKey ?? ""}
+                    width={400}
+                    height={400}
+                  />
+                ) : null}
+              </CardContent>
+              <CardFooter>
+                {d.link ? (
+                  <Link
+                    target="_blank"
+                    className="hover:underline text-blue-400"
+                    href={d.link}
+                  >
+                    Link &gt;
+                  </Link>
+                ) : null}
+              </CardFooter>
+            </ScrollArea>
+          </Card>
+        )
+      }}
+    />
   )
 }
