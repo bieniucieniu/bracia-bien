75086b4505838ce4a52b776e8bde94e72ecad6f2
bieniucieniu <mikibien@outlook.com>
2023-09-20 11:20:43 +0200

    change type source / add deep copy

--- components/Admin/AdminContext.tsx
+++ components/Admin/AdminContext.tsx
@@ -1,27 +1,41 @@
 "use client"
-import type { populateImagesDataWithLinks } from "@/db/imagesData/serverApi"
+import type { insertImagesSchema } from "@/db/imagesData/serverApi"
+import type { insertCardSchema } from "@/db/infoCard/serverApi"
 import { createContext, useContext, useState } from "react"
+import { z } from "zod"
 
-type ImagesData = Awaited<ReturnType<typeof populateImagesDataWithLinks>>
+type ImagesData = (z.infer<typeof insertImagesSchema> & { url?: string })[]
+type InfoCards = z.infer<typeof insertCardSchema>[]
 
 type AdminContext = {
   imagesData: ImagesData
   setImagesData: React.Dispatch<React.SetStateAction<ImagesData>>
+  infoCards: InfoCards
+  setInfoCards: React.Dispatch<React.SetStateAction<InfoCards>>
 }
 
 const adminContext = createContext<AdminContext | null>(null)
 
 export function AdminContextProvider({
   imagesData: newImagesData,
+  infoCards: newInfoCards,
   children,
 }: {
   imagesData: ImagesData | undefined | null
+  infoCards: InfoCards | undefined | null
   children: React.ReactNode
 }) {
-  const [imagesData, setImagesData] = useState<ImagesData>(newImagesData ?? [])
+  const [imagesData, setImagesData] = useState<ImagesData>(
+    () => structuredClone(newImagesData) ?? [],
+  )
+  const [infoCards, setInfoCards] = useState<InfoCards>(
+    () => structuredClone(newInfoCards) ?? [],
+  )
 
   return (
-    <adminContext.Provider value={{ imagesData, setImagesData }}>
+    <adminContext.Provider
+      value={{ imagesData, setImagesData, infoCards, setInfoCards }}
+    >
       {children}
     </adminContext.Provider>
   )
