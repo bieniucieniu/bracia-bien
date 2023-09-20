5c43c44f4b0d3f4f9360c6c368d2f3a84e4271d9
bieniucieniu <mikibien@outlook.com>
2023-09-20 11:21:45 +0200

    change data fetch and auth to wrapers

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,33 +1,22 @@
 import AuthButton from "@/components/Auth"
-import authOptions from "@/lib/auth"
-import { getServerSession } from "next-auth"
-import { utapi } from "uploadthing/server"
+import { getAuth } from "@/lib/auth"
 import { Uploader } from "@/components/Admin/Uploader"
-import { getAllImagesData } from "@/db/imagesData/serverApi"
+import {
+  getAllImagesData,
+  populateImagesDataWithLinks,
+} from "@/db/imagesData/serverApi"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { CardCreator, CardEditorSlider } from "@/components/Admin/CardDashboard"
-import ImageSelesctor from "@/components/Admin/ImageDataEditor"
+import ImagesDataEditor from "@/components/Admin/ImagesDataEditor"
 import { AdminContextProvider } from "@/components/Admin/AdminContext"
+import { getAllInfoCards } from "@/db/infoCard/serverApi"
 
 export default async function Admin() {
-  const session = await getServerSession(authOptions)
-  const { res: allImages, error } = await getAllImagesData()
-  if (error) console.log(error)
-  const imgsUrls =
-    allImages && allImages.length
-      ? await utapi.getFileUrls(allImages.map((k) => k.key))
-      : []
-  const imgsData =
-    allImages && allImages.length
-      ? allImages.map((e) => {
-          return {
-            url: imgsUrls.find((u) => e.key === u.key)?.url,
-            ...e,
-          }
-        })
-      : []
+  const auth = await getAuth()
+  const imagesData = await populateImagesDataWithLinks(await getAllImagesData())
+  const infoCards = await getAllInfoCards()
 
-  if (!session?.user)
+  if (!auth)
     return (
       <main
         style={{ scrollbarGutter: "stable" }}
@@ -39,7 +28,7 @@ export default async function Admin() {
 
   return (
     <main className="px-10 pt-20 pb-4 flex flex-col gap-4 relative ">
-      <AdminContextProvider imagesData={imgsData}>
+      <AdminContextProvider imagesData={imagesData} infoCards={[]}>
         <Card className="w-fit">
           <CardHeader>
             <CardTitle>Admin dashboard</CardTitle>
@@ -47,7 +36,7 @@ export default async function Admin() {
           <CardContent className="flex items-center justify-around gap-x-3">
             <span>
               zalogowany jako{" "}
-              <mark className="px-1 rounded">{session?.user?.name}</mark>
+              <mark className="px-1 rounded">{auth.user?.name}</mark>
             </span>
             <AuthButton signed className="inline-block self-end" />
           </CardContent>
@@ -74,11 +63,13 @@ export default async function Admin() {
               <CardTitle>Admin dashboard</CardTitle>
             </CardHeader>
             <CardContent>
-              <CardEditorSlider data={[]} />
+              <CardEditorSlider
+                data={infoCards instanceof Array ? infoCards : []}
+              />
             </CardContent>
           </Card>
         </section>
-        <ImageSelesctor imgsData={imgsData} />
+        <ImagesDataEditor />
       </AdminContextProvider>
     </main>
   )
