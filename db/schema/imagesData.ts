5dcc8004e142b121cb8ff31ba37bcbb28c72020c
bieniucieniu <mikibien@outlook.com>
2023-10-24 13:17:08 +0200

    add message in imae schema

--- db/schema/imagesData.ts
+++ db/schema/imagesData.ts
@@ -1,4 +1,4 @@
-import { pgTable, pgEnum, unique, varchar } from "drizzle-orm/pg-core"
+import { pgTable, pgEnum, unique, varchar, text } from "drizzle-orm/pg-core"
 
 export const imagesCategorieEnum = pgEnum("images_categorie", [
   "gallery",
@@ -13,6 +13,7 @@ export const imagesData = pgTable(
     key: varchar("key").primaryKey().notNull(),
     categorie: imagesCategorieEnum("categorie").default("gallery").notNull(),
     alt: varchar("alt").default("").notNull(),
+    message: text("massage"),
   },
   (table) => {
     return {
