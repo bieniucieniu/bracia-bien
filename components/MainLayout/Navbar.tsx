944e74c73e09346ff95c88e2fada1ecd1537b21e
bieniucieniu <mikibien@outlook.com>
2023-11-22 11:33:42 +0100

    fix nav on small screen

--- components/MainLayout/Navbar.tsx
+++ components/MainLayout/Navbar.tsx
@@ -10,44 +10,36 @@ import {
 } from "@/components/ui/navigation-menu"
 import { FacebookIcon, InstagramIcon } from "lucide-react"
 
+const items: { title: string; href: string }[] = [
+  {
+    title: "o nas",
+    href: "/#about",
+  },
+  {
+    title: "informacje",
+    href: "/#info",
+  },
+  {
+    title: "galleria produktów",
+    href: "/#gallery",
+  },
+]
+
 export function Topbar() {
   return (
     <nav className="bg-white fixed top-0 left-0 right-0 z-40 drop-shadow-sm p-1 flex flex-wrap-reverse md:grid md:grid-cols-3">
       <NavigationMenu className="m-auto p-1 md:col-start-2">
-        <NavigationMenuList>
-          <NavigationMenuItem className="hidden md:block">
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link href="/">home</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link href="/#about">on nas</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link href="/#info">informacje</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link href="/#gallery">galleria produktów</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
+        <NavigationMenuList className="flex flex-wrap justify-around sm:justify-normal sm:flex-nowrap">
+          {items.map(({ title, href }) => (
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                asChild
+                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+              >
+                <Link href={href}>{title}</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
         </NavigationMenuList>
       </NavigationMenu>
       <div className="flex flex-row justify-between w-full md:w-auto md:justify-end">
