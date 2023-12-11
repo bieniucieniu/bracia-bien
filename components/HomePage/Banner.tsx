91bb1762bdad99fd994c0099b510f21412b276a5
bieniucieniu <mikibien@outlook.com>
2023-12-11 12:35:04 +0100

    image position fixed

--- components/HomePage/Banner.tsx
+++ components/HomePage/Banner.tsx
@@ -5,7 +5,8 @@ import Slider from "../Slider"
 
 const panels: React.ReactNode[] = [
   <div key="panel-lama" className="h-full grid items-center relative">
-    <article className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 drop-shadow-xl backdrop-blur-md py-6 px-8 rounded-xl flex flex-col items-center">
+    <article className="sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 drop-shadow-xl backdrop-blur-md sm:py-6 px-8 rounded-xl flex flex-col items-center">
+      {" "}
       <h1 className="text-4xl font-bold pb-4">Lorem ipsum dolor sit amet</h1>
       <p>
         consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
@@ -18,13 +19,15 @@ const panels: React.ReactNode[] = [
       src="/lama.webp"
       width={1920}
       height={953}
-      className="object-contain rounded-lg w-full h-auto max-w-screen-2xl mx-auto pointer-events-none"
+      className="object-contain rounded-lg max-w-screen-2xl mx-auto pointer-events-none w-full h-auto sm:w-auto sm:h-full sm:max-h-[calc(100svh_-_60px)]"
     />
   </div>,
   <div key="panel-moraj" className="h-full grid items-center relative">
-    <article className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 drop-shadow-xl backdrop-blur-md py-6 px-8 rounded-xl flex flex-col items-center">
-      <h1 className="text-4xl font-bold pb-4">Lorem ipsum dolor sit amet</h1>
-      <p>
+    <article className="sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 drop-shadow-xl backdrop-blur-md sm:py-6 px-8 rounded-xl flex flex-col items-center">
+      <h1 className="text-lg sm:text-4xl font-bold pb-4">
+        Lorem ipsum dolor sit amet
+      </h1>
+      <p className="text-sm">
         consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -35,11 +38,11 @@ const panels: React.ReactNode[] = [
       src="/moraj.webp"
       width={1500}
       height={1080}
-      className="object-contain rounded-lg w-full h-auto max-w-screen-2xl mx-auto pointer-events-none"
+      className="object-contain rounded-lg max-w-screen-2xl mx-auto pointer-events-none w-full h-auto  sm:max-w-[90vw]"
     />
   </div>,
   <div key="panel-gabriella" className="h-full grid items-center relative">
-    <article className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 drop-shadow-xl backdrop-blur-md py-6 px-8 rounded-xl flex flex-col items-center">
+    <article className="sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 drop-shadow-xl backdrop-blur-md sm:py-6 px-8 rounded-xl flex flex-col items-center">
       <h1 className="text-4xl font-bold pb-4">Lorem ipsum dolor sit amet</h1>
       <p>
         consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
@@ -52,7 +55,7 @@ const panels: React.ReactNode[] = [
       src="/gabriella.webp"
       width={1062}
       height={765}
-      className="object-contain rounded-lg w-full h-auto max-w-screen-2xl mx-auto pointer-events-none"
+      className="object-contain rounded-lg max-w-screen-2xl mx-auto pointer-events-none w-full h-auto sm:max-w-[90vw]"
     />
   </div>,
   <div
