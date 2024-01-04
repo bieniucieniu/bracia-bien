8503f399d9fe15d0c9b5e05ef7eb80ada8d9d73e
bieniucieniu <mikibien@outlook.com>
2024-01-04 15:09:48 +0100

    add kunitex banner / change position and styling of text on panels

--- components/HomePage/Banner.tsx
+++ components/HomePage/Banner.tsx
@@ -1,16 +1,21 @@
 "use client"
 import Image from "next/image"
 import { useEffect, useState } from "react"
-import Slider from "../Slider"
+import Slider from "@/components/Slider"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 const panels: React.ReactNode[] = [
   <div key="panel-lama" className="h-full grid items-center relative">
-    <article className="sm:text-white sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 drop-shadow-xl backdrop-blur-md sm:py-6 px-8 rounded-xl flex flex-col items-center">
-      <h1 className="text-3xl font-bold pb-4">
-        Oferujemy szeroki wybór bielizny damskiej, jak i meskiej.
-      </h1>
-      <p className=" w-full">takich marek jak lama czy moraj</p>
-    </article>
+    <Card className="sm:absolute sm:top-3/4 sm:left-3/4 sm:-translate-x-1/2 sm:-translate-y-1/2">
+      <CardHeader>
+        <CardTitle>
+          Oferujemy szeroki wybór bielizny damskiej, jak i meskiej.
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">takich marek jak lama czy moraj</p>
+      </CardContent>
+    </Card>
     <Image
       alt={`panel-lama`}
       src="/lama.webp"
@@ -20,15 +25,17 @@ const panels: React.ReactNode[] = [
     />
   </div>,
   <div key="panel-moraj" className="h-full grid items-center relative">
-    <article className="sm:absolute sm:top-1/3 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 drop-shadow-xl backdrop-blur-md sm:py-6 px-8 rounded-xl flex flex-col items-center">
-      <h1 className="text-lg sm:text-4xl font-bold pb-4">
-        U nas znajdzesz piżamę dla siebie
-      </h1>
-      <p className="text-sm">
-        Szeroka oferta piżam od Moraj, Lama, Martel, Szata, M-max i Kuba w wielu
-        rozmiarch (xs-6xl)
-      </p>
-    </article>{" "}
+    <Card className="sm:absolute sm:top-20 sm:left-1/4 sm:-translate-x-1/2 sm:-translate-y-1/2">
+      <CardHeader>
+        <CardTitle>U nas znajdzesz piżamę dla siebie</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">
+          Szeroka oferta piżam od Moraj, Lama, Martel, Szata, M-max i Kuba w
+          wielu rozmiarch (xs-6xl)
+        </p>
+      </CardContent>
+    </Card>
     <Image
       alt={`panel-moraj`}
       src="/moraj.webp"
@@ -38,14 +45,16 @@ const panels: React.ReactNode[] = [
     />
   </div>,
   <div key="panel-gabriella" className="h-full grid items-center relative">
-    <article className="sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 drop-shadow-xl backdrop-blur-md sm:py-6 px-8 rounded-xl flex flex-col items-center">
-      <h1 className="text-4xl font-bold pb-4">
-        Posiadamy w ofercie szeroki wybór rajstop
-      </h1>
-      <p className="w-full">
-        Wiele wzorow, kolorow jak i matowe czy zpolyskiem
-      </p>
-    </article>
+    <Card className="sm:absolute sm:top-3/4 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2">
+      <CardHeader>
+        <CardTitle>Posiadamy w ofercie szeroki wybór rajstop</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">
+          Wiele wzorow, kolorow jak i matowe czy zpolyskiem
+        </p>
+      </CardContent>
+    </Card>
     <Image
       alt={`panel-gabriella`}
       src="/gabriella.webp"
@@ -60,11 +69,11 @@ const panels: React.ReactNode[] = [
   >
     <article>
       <h1 className="text-4xl font-bold pb-4">
-        Bielizne tych marek znajdziesz u nas
+        Bielizne i pizamy tych marek znajdziesz u nas
       </h1>
       <sup className="text-sm text-muted-foreground">
         Moraj, Gabriella, Lama, Noviti, Szata, Wik, Martel, M-max, Hodberg,
-        Lavaza, Kunitex, Tejrax
+        Kunitex, Tejrax
       </sup>
     </article>
     <ul className="grid grid-cols-4 justify-center items-center gap-2">
@@ -72,6 +81,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/moraj.png"
           alt="moraj_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -81,6 +91,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/gabriella.png"
           alt="gabriella_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -90,6 +101,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/lama.png"
           alt="lama_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -99,6 +111,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/szata.png"
           alt="szata_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -108,6 +121,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/noviti.jpg"
           alt="noviti_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -117,6 +131,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/wik.png"
           alt="wik_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -126,6 +141,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/mmax.png"
           alt="m-max_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -135,6 +151,7 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/martel.png"
           alt="martel_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
@@ -144,6 +161,17 @@ const panels: React.ReactNode[] = [
         <Image
           src="/logos/hodberg.jpeg"
           alt="hodberg_logo"
+          priority
+          width={150}
+          height={100}
+          className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
+        />
+      </li>
+      <li>
+        <Image
+          src="/logos/kunitex.webp"
+          alt="kunitex_logo"
+          priority
           width={150}
           height={100}
           className="object-contain rounded-lg pointer-events-none h-[100px] w-auto max-w-[150px] bg-white mx-auto"
