c06374e841d46c7269c26e6efa0093205b2d08d6
bieniucieniu <mikibien@outlook.com>
2024-01-31 14:12:57 +0100

    add ssr plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { roboto } from "@/lib/fonts"
 import Navbar from "@/components/MainLayout/Navbar"
 import { AnimationProvider } from "@/components/MainLayout/Animation"
 import { ThemeProvider } from "@/components/MainLayout/ThemeProvider"
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin"
+import { extractRouterConfig } from "uploadthing/server"
+import { fileRouter } from "@/app/api/uploadthing/core"
 
 export const metadata = {
   title: "Bracia Bien",
@@ -24,6 +27,7 @@ export default async function RootLayout({
           disableTransitionOnChange
         >
           <AnimationProvider>
+            <NextSSRPlugin routerConfig={extractRouterConfig(fileRouter)} />
             <Navbar />
             {children}
           </AnimationProvider>
